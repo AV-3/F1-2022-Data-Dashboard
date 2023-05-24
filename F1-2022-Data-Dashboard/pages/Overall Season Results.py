ea936bf0a5f0d97af8338a7bdf8aad0b46d6eb7f
Alan Velez <45529869+AV-3@users.noreply.github.com>
2023-05-24 15:02:44 -0500

    Added multi page functionality
    finished the landing page section, added image folder, pages folder, and styles.css

---
+++ F1-2022-Data-Dashboard/pages/Overall Season Results.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+
+with open("styles.css") as css:
+    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+
+#Import csv files
+df1 = pd.read_csv("csv/drivers.csv")
+df2 = pd.read_csv("csv/teams.csv")
+df3 = pd.read_csv("csv/races.csv")
+df4 = pd.read_csv("csv/GrandPrix_FastestLaps.csv")
+df5 = pd.read_csv("csv/GrandPrix_Qualifying.csv")
+df6 = pd.read_csv("csv/GrandPrix_RaceData.csv")
+
+
+#Sidebar functionality
+st.sidebar.title("F1 Dashboard")
+st.sidebar.title("Filters")
+
+# Add a dropdown to select the race
+grandprix_dropdown = st.sidebar.selectbox("Grand Prix", df3["Grand Prix"].unique())
+
+# Add a dropdown to select the team
+team_dropdwon = st.sidebar.selectbox("Team", df2["Team"].unique())
+
+# Add a dropdown to select the driver
+driver_dropdown = st.sidebar.selectbox("Driver", df1["Driver"].unique())
+
+# Add a checkbox to show only races where the driver finished in the top 5
+top_5_checkbox = st.sidebar.checkbox("Show podium results only")
+
+# Create a main page
+st.title("Formula 1 2022 Season Statistics")
+
+# Show the driver's name and team
+st.write(f"Grand Prix: {grandprix_dropdown}")
+st.write(f"Team: {df1[df1['Driver'] == driver_dropdown]['Driver'].values[0]}")
+
+# Show the driver's statistics
+st.table(df1[df1['Driver'] == driver_dropdown].drop(['Nationality','Car', 'PTS'], axis=1))
+
+# If the "Show only races where the driver finished in the top 5" checkbox is checked, only show the races where the driver finished in the top 5
+if top_5_checkbox:
+    df = df1[df1['Driver'] == driver_dropdown]
+    df = df[df['POS'] <= 5]
+
+# Show a table of the driver's race results
+st.table(df6[['Pos', 'Driver', 'Car', 'Laps', 'Time/Retired', 'PTS']])
